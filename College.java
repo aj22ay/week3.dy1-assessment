6526f42d018225e1e861ec5ae9d77d3e4077b2eb
aj22ay <90094851+aj22ay@users.noreply.github.com>
2021-09-23 20:51:23 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,14 @@
+package week3.day1;
+
+public class College {
+
+	public void collegeName() {
+		System.out.println("Mkce");
+	}
+	public void collegeCode() {
+		System.out.println("Mkce16");
+	}
+	public void collegeRank() {
+		System.out.println("c");
+	}
+}
