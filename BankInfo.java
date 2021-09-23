cbc011a705f6ab194739f07bee6c82b6ee9bb9e6
aj22ay <90094851+aj22ay@users.noreply.github.com>
2021-09-23 22:21:08 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,14 @@
+package week3.day1;
+
+public class BankInfo {
+
+	public void saving() {
+		System.out.println("saving account");
+	}
+	public void fixed() {
+		System.out.println("fixed details");
+	}
+	public void deposit() {
+		System.out.println("deposit amount");
+	}
+}
